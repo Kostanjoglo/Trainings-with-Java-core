311696bc917fe2f3aa96fcce30599e9710c79bb4
Serhii Blynov <Serhii_Blynov@epam.com>
2018-08-07 16:07:37 +0300

    CONTINUE WITH TRAININGS

---
+++ src/com/company/Producer.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Random;
+
+public class Producer implements Runnable {
+    private Drop drop;
+
+    public Producer(Drop drop) {
+        this.drop = drop;
+    }
+
+    public void run() {
+        String importantInfo[] = {
+                "Mares eat oats",
+                "Does eat oats",
+                "Little lambs eat ivy",
+                "A kid will eat ivy too"
+        };
+        Random random = new Random();
+
+        for (String anImportantInfo : importantInfo) {
+            drop.put(anImportantInfo);
+            try {
+                Thread.sleep(random.nextInt(5000));
+            } catch (InterruptedException e) {
+            }
+        }
+        drop.put("DONE");
+    }
+}
